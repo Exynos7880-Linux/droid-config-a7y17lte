77296d0353959bd00d015a6353edd3a9e8a481e2
aymanrgab <aymanrar2c@gmail.com>
2024-03-24 21:22:02 +0200

    update

--- sparse/usr/bin/droid/droid-hal-early-init.sh
+++ sparse/usr/bin/droid/droid-hal-early-init.sh
@@ -3,10 +3,14 @@
 if ! grep -q hybris /system/etc/ld.config.29.txt; then
     mount -o bind /usr/libexec/droid-hybris/system/etc/ld.config.29.txt /system/etc/ld.config.29.txt
 fi
+mount -o bind /opt/vendor/etc/init/init.samsungexynos7880.rc /vendor/etc/init/hw/init.samsungexynos7880.rc
 mount -o bind /system/lib64/hw/audio.hidl_compat.default.so /vendor/lib64/hw/audio.primary.default.so
+#mount -o bind /dev/null /vendor/etc/init/hw/init.samsungexynos7880.usb.rc
+mknod /dev/tun c 10 200
+chmod 660 /dev/tun
+chown system:vpn /dev/tun
 
 if [[ ! -e /mnt/vendor ]]; then
     mkdir -p /mnt/vendor
     ln -s /efs /mnt/vendor/
 fi
-
---
+++ sparse/var/lib/platform-updates/flash-bootimg.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+/usr/sbin/flash-partition BOOT /boot/hybris-boot.img
